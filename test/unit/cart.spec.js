27360f70c8598bd28289897ef2ce22b4914cd923
Reza Salimian <reza@jewelscent.com>
2018-10-08 18:18:03 -0500

    removes a sticker from the cart - using remove funciton

--- test/unit/cart.spec.js
+++ test/unit/cart.spec.js
@@ -7,39 +7,48 @@ let product;
 
 describe('Shopping cart', function() {
 
-  describe('shopping cart model', () => {
+    describe('shopping cart model', () => {
 
-    beforeEach(() => {
-      cart = new Cart({});
-      product = new Product({
-        "imagePath": "https://buildahead.com/wp-content/uploads/2017/02/happy-emoji-smaller.png",
-        "title": "Happy",
-        "description": "Happy",
-        "price":5});
-    });
+        beforeEach(() => {
+            cart = new Cart({});
+            product = new Product({
+                "imagePath": "https://buildahead.com/wp-content/uploads/2017/02/happy-emoji-smaller.png",
+                "title": "Happy",
+                "description": "Happy",
+                "price": 5
+            });
+        });
 
-    it('adds a sticker to the cart', function() {
-      cart.add(product, product.id);
-      assert.equal(cart.totalPrice, 5);
-    });
+        it('adds a sticker to the cart', function() {
+            cart.add(product, product.id);
+            assert.equal(cart.totalPrice, 5);
+        });
 
-    it('removes a sticker from the cart', function() {
-      cart.add(product, product.id);
-      cart.reduceByOne(product.id);
-      assert.deepEqual(cart.items, {});
-      assert.equal(cart.totalPrice, 0);
-    });
+        it('removes a sticker from the cart', function() {
+            cart.add(product, product.id);
+            cart.reduceByOne(product.id);
+            assert.deepEqual(cart.items, {});
+            assert.equal(cart.totalPrice, 0);
+        });
 
-    it('remove all quantities of sticker from the cart', function() {
-      cart.add(product, product.id);
-      cart.add(product, product.id);
-      cart.removeItem(product.id);
-      assert.deepEqual(cart.items, {});
-      assert.equal(cart.totalPrice, 0);
-    });
+        it('remove all quantities of sticker from the cart', function() {
+            cart.add(product, product.id);
+            cart.add(product, product.id);
+            cart.removeItem(product.id);
+            assert.deepEqual(cart.items, {});
+            assert.equal(cart.totalPrice, 0);
+        });
 
-    it('returns an empty array', function() {
-      assert.deepEqual(cart.generateArray(),[]);
+        it('returns an empty array', function() {
+            assert.deepEqual(cart.generateArray(), []);
+        });
+
+        it('removes a sticker from the cart - using remove funciton', function() {
+            cart.add(product, product.id);
+            cart.removeItem(product.id);
+            assert.deepEqual(cart.items, {});
+            assert.equal(cart.totalPrice, 0);
+        });
+
     });
-  });
 });
